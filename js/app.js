22c54ab3a40d63d057c53dbb626c5152af1f7bc5
wangshijun2010 <wangshijun2010@gmail.com>
2014-04-01 15:18:35 +0800

    add contacts page

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ var app = angular.module('app', []);
 app.controller('HelloController', function ($scope) {
     $scope.name = 'Xuan Xuan';
 });
+
+app.controller('ContactController', function ($scope) {
+    $scope.contacts = [
+        { name: 'Shijun', mobile: 15811057083 },
+        { name: 'Jingwei', mobile: 13888888888 },
+    ];
+
+});
