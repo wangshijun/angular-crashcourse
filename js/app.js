41edd38b8748bf85e27590367241b5955f423188
wangshijun2010 <wangshijun2010@gmail.com>
2014-04-01 17:47:27 +0800

    contact book

--- js/app.js
+++ js/app.js
@@ -8,8 +8,31 @@ app.controller('HelloController', function ($scope) {
 
 app.controller('ContactController', function ($scope) {
     $scope.contacts = [
-        { name: 'Shijun', mobile: 15811057083 },
-        { name: 'Jingwei', mobile: 13888888888 },
+        { name: '仕军', mobile: 15811057083 },
+        { name: '敬威', mobile: 13888888888 },
     ];
+
+    $scope.addContact = function () {
+
+        var hasDuplicate = false;
+        $scope.contacts.forEach(function (contact) {
+            if (contact.name === $scope.name) {
+                hasDuplicate = true;
+            }
+        });
+
+        if (hasDuplicate) {
+            alert('Duplicate contact name');
+        } else {
+            $scope.contacts.push({
+                name: $scope.name,
+                mobile: $scope.mobile,
+            });
+        }
+    };
+
+    $scope.deleteContact = function () {
+        $scope.contacts.splice(this.$index, 1);
+    };
 
 });
